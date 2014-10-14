4d659f2e421fe335ff7bcbbe42fc2898f365c977
TubalLara <tubal.lara@tajamar365.com>
2014-10-14 10:33:47 +0200

    más func

--- WebSocketTest/js/websocket.js
+++ WebSocketTest/js/websocket.js
@@ -19,10 +19,11 @@ function escribirLista(obj) {
 }
 
 function mensaje(evt) {
-    //el JSON con el que trbajamos con el servidor tiene la propiedad text y la propiedad id, a mí me interesa la propiedad text
+    //el JSON con el que trabajamos con el servidor tiene la propiedad text y la propiedad id, a mí me interesa la propiedad text
     var datos = eval('(' + evt.data + ')');
-
-    if (datos.length > 0) {
+    //le pido que me mida los datos, los evalúe, me mida si la longitud es mayor de cero, es decir si es un array, y si no lo es que me lo escriba, sino que me haga una lista
+    if (datos.length) {
+        //le pregunto si existe la propiedad lenght, para ver si es un array de objetos o un json
         for (var i = 0; i < datos.length; i++) {
 
             escribirLista(datos[i]);
@@ -31,6 +32,19 @@ function mensaje(evt) {
     } else {
         escribirLista(datos);
     }
-
-
+    
 }
+
+(function () {
+    socket = new WebSocket("ws://localhost:54611/socket.ashx");
+    socket.onopen = function() {
+        alert("Abierto");
+    };
+    socket.onmessage = mensaje;
+    socket.onerror = function(err) {
+
+        alert("Cerrado");
+
+    };
+
+})();
